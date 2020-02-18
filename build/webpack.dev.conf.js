9e059e9090a1021a3fb4a8e9e279680a21f240e2
shuangliz <358355029@qq.com>
2020-02-18 11:53:50 +0800

    improve the host setting

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -22,7 +22,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
-    clientLogLevel: 'warning',
+      disableHostCheck: true,
+      clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
